aa70b47da1e2fc76226eda04dd87231de28e0d3f
MattW-12106 <12106@holbertonstudents.com>
2026-01-23 15:50:18 +1100

    prints all numbers from 0 to 98 in decimal and hexadecimal

---
+++ python-if_else_loops_functions/4-print_hexa.py
@@ -0,0 +1,3 @@
+#!/usr/bin/python3
+for i in range(99): # Print all numbers from 0 to 98 in hexadecimal format
+    print("{} = {}".format(i, hex(i))) # Print the number and its hexadecimal representation
