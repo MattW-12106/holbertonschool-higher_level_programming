2f5751e1a3b3dbe92f3bbb862cf145b54403f4eb
MattW-12106 <12106@holbertonstudents.com>
2026-01-23 07:01:15 +0000

    removed explanation line bc it counted the print in it as a command

--- python-if_else_loops_functions/2-print_alphabet.py
+++ python-if_else_loops_functions/2-print_alphabet.py
@@ -1,3 +1,3 @@
 #!/usr/bin/python3
 for i in range(97, 123): #iterates through ASCII values of a-z
-    print("%c" % i, end="") #python conversion specifier to print character + end="" to avoid new line
+    print("%c" % i, end="")
