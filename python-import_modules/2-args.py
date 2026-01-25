b444b433e82b7a60584122f7e48ad53f51f81937
MattW-12106 <12106@holbertonstudents.com>
2026-01-25 20:50:48 +1100

    counts the amount of arguments

---
+++ python-import_modules/2-args.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+import sys
+
+if __name__ == "__main__":
+    argc = len(sys.argv) - 1
+
+    if argc == 0:
+        print("Number of argument(s):.")
+    elif argc == 1:
+        print("Number of argument(s): 1 argument:")
+    else:
+        print("Number of argument(s): {} arguments:".format(argc))
+
+    for i in range(argc):
+        print("{}: {}".format(i + 1, sys.argv[i + 1]))
