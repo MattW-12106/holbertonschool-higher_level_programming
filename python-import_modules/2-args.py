d33f56a9121289b15ac40d1701ab064e1f2f6723
MattW-12106 <12106@holbertonstudents.com>
2026-01-25 21:00:19 +1100

    corrected formatting for arg counter

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -5,11 +5,11 @@ if __name__ == "__main__":
     argc = len(sys.argv) - 1
 
     if argc == 0:
-        print("Number of argument(s):.")
+        print("0 arguments.")
     elif argc == 1:
-        print("Number of argument(s): 1 argument:")
+        print("1 argument:")
     else:
-        print("Number of argument(s): {} arguments:".format(argc))
+        print("{} arguments:".format(argc))
 
     for i in range(argc):
         print("{}: {}".format(i + 1, sys.argv[i + 1]))
