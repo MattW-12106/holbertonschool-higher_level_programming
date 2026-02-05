c95232524e9772167b1ef0bedc1c27e26d1aab39
MattW-12106 <12106@holbertonstudents.com>
2026-02-05 12:23:55 +1100

    docstring wasnt a function inside the function file so I added the function

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -1,8 +1,13 @@
 #!/usr/bin/python3
-"""
-This module defines a function that adds two integers.
-"""
 def add_integer(a, b=98):
+    """
+    Adds two integers.
+
+    >>> add_integer = __import__('0-add_integer').add_integer
+    >>> add_integer("a", 1)
+    Traceback (most recent call last):
+    TypeError: a must be an integer
+    """
     if not isinstance(a, (int, float)):
         raise TypeError("a must be an integer")
     if not isinstance(b, (int, float)):
