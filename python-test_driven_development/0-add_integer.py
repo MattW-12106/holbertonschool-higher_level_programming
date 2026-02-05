c6c39fd386b939d582da4d20214ec4e427498fd4
MattW-12106 <12106@holbertonstudents.com>
2026-02-05 22:54:08 +1100

    doctest fixed now?

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -1,14 +1,12 @@
 #!/usr/bin/python3
-"""
-Adds two integers.
-
->>> add_integer = __import__('0-add_integer').add_integer
->>> add_integer("a", 1)
-Traceback (most recent call last):
-TypeError: a must be an integer
-"""
-
 def add_integer(a, b=98):
+    """
+    Adds two integers.
+    >>> add_integer = __import__('0-add_integer').add_integer
+    >>> add_integer("a", 1)
+    Traceback (most recent call last):
+    TypeError: a must be an integer
+    """
     if not isinstance(a, (int, float)):
         raise TypeError("a must be an integer")
     if not isinstance(b, (int, float)):
