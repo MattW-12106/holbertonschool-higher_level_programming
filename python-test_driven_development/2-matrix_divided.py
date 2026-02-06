b6464f780f799a7e0160d174754f0f4940b1741f
MattW-12106 <12106@holbertonstudents.com>
2026-02-06 20:15:57 +1100

    PLEASE WORKK

--- python-test_driven_development/2-matrix_divided.py
+++ python-test_driven_development/2-matrix_divided.py
@@ -2,30 +2,32 @@
 """Defines a function that divides all elements of a matrix."""
 def matrix_divided(matrix, div):
     """
-    Divides all elements of a matrix.
-    >>> matrix = [[1, 2, 3], [4, 5, 6]]
+    matrix = [[1, 2, 3], [4, 5, 6]]
     >>> matrix_divided(matrix, 3)
     [[0.33, 0.67, 1.0], [1.33, 1.67, 2.0]]
-    >>> matrix_divided(matrix, 0)
-    Traceback (most recent call last):
-    ZeroDivisionError: division by zero
-    >>> matrix_divided(matrix, "3")
-    Traceback (most recent call last):
-    TypeError: div must be a number
-    >>> matrix_divided([[1, 2], [3, 4, 5]], 2)
+    >>> matrix = [[1, 2, 3], [4]]
+    >>> matrix_divided(matrix, 3)
     Traceback (most recent call last):
     TypeError: Each row of the matrix must have the same size
-    >>> matrix_divided([[1, 2], [3, "4"]], 2)
+    >>> matrix = [[1, 2, 3], [4, 5, 6]]
+    >>> matrix_divided(matrix, 'nan')
     Traceback (most recent call last):
-    TypeError: matrix must be a matrix (list of lists) of integers/floats
-    >>> matrix_divided(matrix, float('inf'))
-    [[0.0, 0.0, 0.0], [0.0, 0.0, 0.0]]
+    TypeError: div must be a number
+       >>> matrix_divided(matrix, 0)
+    Traceback (most recent call last):
+    ZeroDivisionError: division by zero
     >>> matrix_divided(matrix, )
     Traceback (most recent call last):
     TypeError: matrix_divided() missing 1 required positional argument: 'div'
-    >>> matrix_divided( )
+    >>> matrix_divided()
     Traceback (most recent call last):
-    TypeError: matrix_divided() missing 2 required positional arguments: 'matrix' and 'div'
+    TypeError: matrix_divided() missing 2 required positional arguments:
+      'matrix' and 'div'
+    >>> matrix_divided(matrix, float('inf'))
+    [[0.0, 0.0, 0.0], [0.0, 0.0, 0.0]]
+    >>> matrix_divided('derp', 1)
+    Traceback (most recent call last):
+    TypeError: matrix must be a matrix (list of lists) of integers/floats
     """
     if not isinstance(matrix, list) or not all(isinstance(row, list) for row in matrix):
         raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
