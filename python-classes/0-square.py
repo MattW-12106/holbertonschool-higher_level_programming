1605e89b8ab35ce3b9e0c0f1155d09a296bc6cd7
MattW-12106 <12106@holbertonstudents.com>
2026-02-07 20:30:46 +1100

    ...i overdid the first question and made it able to have different sizes instead of just making it and doing nothing else with it

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,19 +1,4 @@
 #!/usr/bin/python3
 class Square:
-    """
-    Create a square with default size:
-    >>> s = Square()
-    >>> isinstance(s, Square)
-    True
-
-    Create a square with a given size:
-    >>> s = Square(5)
-    >>> isinstance(s, Square)
-    True
-
-    Private attribute exists:
-    >>> hasattr(s, "_Square__size")
-    True
-    """
-    def __init__(self, size=0):
-        self.__size = size
+    """A class that defines a square."""
+    pass
