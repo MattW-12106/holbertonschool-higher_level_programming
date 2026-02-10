9eae42cf505e26162f25b29d13412d843e4a97c6
MattW-12106 <12106@holbertonstudents.com>
2026-02-10 14:28:26 +1100

    added height setter and property for height

--- python-more_classes/1-rectangle.py
+++ python-more_classes/1-rectangle.py
@@ -10,17 +10,36 @@ class Rectangle:
         """
         self.width = width
         self.height = height
+    
     @property
     def width(self):        
         """Get the width of the rectangle."""
         return self.__width
+    
     @width.setter
+    def width(self, value):
+        """Set the width of the rectangle.
+           value: The width value to set.
+        """
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+    
+    @property
+    def height(self):        
+        """Get the height of the rectangle."""
+        return self.__height
+    
+    @height.setter
     def height(self, value):
         """Set the height of the rectangle.
-           value (int): The height value to set.
+           value: The height value to set.
         """
         if not isinstance(value, int):
             raise TypeError("height must be an integer")
         if value < 0:
             raise ValueError("height must be >= 0")
         self.__height = value
+    
