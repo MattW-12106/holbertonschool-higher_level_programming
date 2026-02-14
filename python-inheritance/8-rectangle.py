7769dd501964658a04f1862745c3bf11b5e55996
MattW-12106 <12106@holbertonstudents.com>
2026-02-14 20:11:33 +1100

    new class rectangle which inherits from BaseGeometry

---
+++ python-inheritance/8-rectangle.py
@@ -0,0 +1,49 @@
+#!/usr/bin/python3
+"""defines an empty class BaseGeometry with inheritance with Rectangle"""
+class BaseGeometry:
+    """
+    >>> bg = BaseGeometry()
+    >>> bg.area()
+    Traceback (most recent call last):
+    Exception: area() is not implemented
+
+    >>> bg.integer_validator("width", 10)
+    >>> bg.integer_validator("width", 0)
+    Traceback (most recent call last):
+    ValueError: width must be greater than 0
+
+    >>> bg.integer_validator("width", -3)
+    Traceback (most recent call last):
+    ValueError: width must be greater than 0
+
+    >>> bg.integer_validator("width", "4")
+    Traceback (most recent call last):
+    TypeError: width must be an integer
+    """
+    def area(self):
+        """raises an exception with the message area() is not implemented"""
+        raise Exception("area() is not implemented")
+    def integer_validator(self, name, value):
+        """validates value"""
+        if type(value) != int:
+            raise TypeError("{} must be an integer".format(name))
+        if value <= 0:
+            raise ValueError("{} must be greater than 0".format(name))
+
+"""defines a class Rectangle that inherits from BaseGeometry"""
+
+class Rectangle(BaseGeometry):
+    """
+    >>> r = Rectangle(3, 4)
+    >>> r.area()
+    12
+
+    >>> r.integer_validator("width", 10)
+    >>> r.integer_validator("height", 20)
+    """
+    def __init__(self, width, height):
+        """initializes width and height"""
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
+        self.__width = width
+        self.__height = height
