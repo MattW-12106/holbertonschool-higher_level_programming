c0e5c750df64b638d828cf585efb257159ae70c7
MattW-12106 <12106@holbertonstudents.com>
2026-02-17 20:44:01 +1100

    indentation was wrong

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -1,16 +1,15 @@
 #!/usr/bin/python3
 """Write a script that adds all arguments to a Python list, and then save them to a file"""
-import json
 import sys
 
 if __name__ == "__main__": # no ModuleNotFoundError when importing this file now
     save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
     load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
-filename = "add_item.json"
-try:
-    my_list = load_from_json_file(filename)
-except FileNotFoundError:
-    my_list = []
-my_list.extend(sys.argv[1:])
-save_to_json_file(my_list, filename)
+    filename = "add_item.json"
+    try:
+        my_list = load_from_json_file(filename)
+    except FileNotFoundError:
+        my_list = []
+    my_list.extend(sys.argv[1:])
+    save_to_json_file(my_list, filename)
