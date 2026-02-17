b7d10b1037f2e3a2d225917fe41365c1babf6587
MattW-12106 <12106@holbertonstudents.com>
2026-02-17 14:24:50 +1100

    read file and prints it to stdout

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -2,6 +2,6 @@
 """This module provides a function to read and print the contents of a file."""
 def read_file(filename="my_file_0.txt"):
     """Reads a file and prints its contents."""
-    with open(filename, 'r') as file:
-        contents = file.read()
-        print(contents)
+    with open(filename, encoding="utf-8") as f:
+        for line in f:
+            print(line, end="")
