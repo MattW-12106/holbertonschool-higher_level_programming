61a09c04e44128dd9b42f845d397ff7e23af940b
MattW-12106 <12106@holbertonstudents.com>
2026-02-17 20:41:56 +1100

    Write a script that adds all arguments to a Python list, and then save them to a file

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
-
 """Write a script that adds all arguments to a Python list, and then save them to a file"""
 import json
+import sys
 
-save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
-load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
+if __name__ == "__main__": # no ModuleNotFoundError when importing this file now
+    save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+    load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
 filename = "add_item.json"
 try:
