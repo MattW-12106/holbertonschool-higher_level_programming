1fe46015030e35a6f6fab715f537dd389da95396
MattW-12106 <12106@holbertonstudents.com>
2026-02-18 11:59:32 +1100

    additions to student class

--- python-input_output/11-student.py
+++ python-input_output/11-student.py
@@ -4,8 +4,9 @@ Write a class Student that defines a student by:
 (based on 10-student.py)
 """
 class Student:
-    def __init__(self, name, age):
-        self.name = name
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
         self.age = age
 
     def to_json(self):
