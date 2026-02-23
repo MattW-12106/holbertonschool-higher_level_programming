509bf629ba5a8c603e5b2679c3b7388893b91732
MattW-12106 <12106@holbertonstudents.com>
2026-02-23 15:58:03 +1100

    hardcoded filename into open function for save function now it will call correctly!

--- restful-api/task_02_requests.py
+++ restful-api/task_02_requests.py
@@ -17,13 +17,13 @@ def fetch_and_print_posts():
     else:
         print(f"Failed to fetch posts.")
 
-def fetch_and_save_posts(filename):
+def fetch_and_save_posts():
     """Fetch posts from JSONPlaceholder and save them to a CSV file"""
     response = requests.get("https://jsonplaceholder.typicode.com/posts")
 
     if response.status_code == 200:
         posts = response.json()
-        with open(filename, mode='w', newline='') as csvfile:
+        with open("posts.csv", mode='w', newline='') as csvfile:
             fieldnames = ['id', 'title', 'body']
             writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
             writer.writeheader()
@@ -33,9 +33,9 @@ def fetch_and_save_posts(filename):
                                  'title': post['title'], 
                                  'body': post['body']
                                  })
-            print(f"Posts saved to {filename}")
+            print(f"Posts saved to {'posts.csv'}.")
     
     # calls required functions
     if __name__ == "__main__":
-        fetch_and_print_posts(filename)
-        fetch_and_save_posts("posts.csv")
+        fetch_and_print_posts()
+        fetch_and_save_posts()
