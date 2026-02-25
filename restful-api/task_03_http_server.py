b00ae197ffc500cebae1374ffdf8eb9ae187349b
MattW-12106 <12106@holbertonstudents.com>
2026-02-25 16:48:40 +1100

    http server

---
+++ restful-api/task_03_http_server.py
@@ -0,0 +1,53 @@
+#!/usr/bin/python3
+"""Develop a simple API using Python with the `http.server` module"""
+
+from http.server import BaseHTTPRequestHandler, HTTPServer
+import json
+
+class SimpleAPIHandler(BaseHTTPRequestHandler):
+    def do_GET(self):
+        """Handle GET requests"""
+
+        if self.path == '/':
+            self.send_response(200)
+            self.send_header('Content-type', 'text/plain')
+            self.end_headers()
+            self.wfile.write(b'Hello, this is a Simple API!')
+
+        # If the path is '/data', return a JSON response with some data
+        elif self.path == '/data':
+            self.send_response(200)
+            self.send_header('Content-type', 'application/json')
+            self.end_headers()
+            data = {
+                "name": "John",
+                "age": 30,
+                "city": "New York"
+            }
+            self.wfile.write(json.dumps(data).encode())
+        
+        elif self.path == "/status":
+            self.send_response(200)
+            self.send_header('Content-type', 'application/json')
+            self.end_headers()
+            status = {
+                "status": "OK"
+            }
+            self.wfile.write(json.dumps(status).encode())
+        
+        # If the path is not there, return a 404 Not Found response
+        else:
+            self.send_response(404)
+            self.send_header('Content-type', 'text/plain')
+            self.end_headers()
+            self.wfile.write(b'Not Found')
+
+def run(server_class=HTTPServer, handler_class=SimpleAPIHandler, port=8000):
+    """Run the HTTP server"""
+    server_address = ('', port)
+    httpd = server_class(server_address, handler_class)
+    print(f'Starting httpd server on port {port}...')
+    httpd.serve_forever()
+
+if __name__ == "__main__":
+    run()
