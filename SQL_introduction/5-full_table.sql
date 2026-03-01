5e860fe75bc8304b5d803b9c691c15b3b902606e
MattW-12106 <12106@holbertonstudents.com>
2026-03-01 23:07:18 +1100

    i overdid it...

--- SQL_introduction/5-full_table.sql
+++ SQL_introduction/5-full_table.sql
@@ -1,10 +1,2 @@
 -- prints description of the table
-SELECT COLUMN_NAME AS Field,
-       COLUMN_TYPE AS Type,
-       IS_NULLABLE AS `Null`,
-       COLUMN_KEY AS `Key`,
-       COLUMN_DEFAULT AS `Default`,
-       EXTRA AS Extra
-FROM information_schema.COLUMNS
-WHERE TABLE_SCHEMA = DATABASE()
-  AND TABLE_NAME = 'first_table';
+SHOW CREATE TABLE first_table;
