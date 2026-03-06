dbe9deb158a06404cc8c58e1efe5f2663b292a62
MattW-12106 <12106@holbertonstudents.com>
2026-03-06 12:38:53 +1100

    created new user

--- SQL_more_queries/1-create_user.sql
+++ SQL_more_queries/1-create_user.sql
@@ -1,8 +1,11 @@
 -- creates the MySQL server user user_0d_1
+
 -- the user is created with the password user_0d_1_pwd
 -- the user is allowed to connect from localhost only
-
-CREATE USER 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
 
 -- grants all privileges to the user user_0d_1
-GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost' WITH GRANT OPTION;
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
+
+-- flushes the privileges to ensure that they are applied
+FLUSH PRIVILEGES;
