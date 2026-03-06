81991be3e9d7e7d53912c1827b63cb654d7df163
MattW-12106 <12106@holbertonstudents.com>
2026-03-06 12:45:53 +1100

    creates the database hbtn_0d_2 and the user user_0d_2

--- SQL_more_queries/2-create_read_user.sql
+++ SQL_more_queries/2-create_read_user.sql
@@ -1,6 +1,11 @@
 -- creates the database hbtn_0d_2 and the user user_0d_2
 IF NOT EXISTS CREATE DATABASE hbtn_0d_2;
-IF NOT EXISTS CREATE USER 'user_0d_2'@'localhost' IDENTIFIED BY 'hbtn_0d_2_pwd';
+
+IF NOT EXISTS CREATE USER 'user_0d_2'@'localhost'
+IDENTIFIED BY 'hbtn_0d_2_pwd';
 
 -- user only has SELECT privilege on the database hbtn_0d_2
-GRANT SELECT ON DATABASE hbtn_0d_2 TO 'user_0d_2'@'localhost';
+GRANT SELECT ON hbtn_0d_2 TO 'user_0d_2'@'localhost';
+
+-- flush privileges to apply changes
+FLUSH PRIVILEGES;
