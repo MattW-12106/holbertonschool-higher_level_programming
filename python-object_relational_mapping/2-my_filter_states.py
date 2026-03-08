478fdf06b3212c52c06556dcad4707a78aacd038
MattW-12106 <12106@holbertonstudents.com>
2026-03-08 22:27:46 +1100

    takes in an argument and displays all values in the states table of hbtn_0e_0_usa where name matches the argument

---
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""
+takes in an argument and displays all values 
+in the states table of hbtn_0e_0_usa where name matches the argument
+"""
+
+# imports
+import MySQLdb
+import sys
+
+if __name__ == "__main__":
+    # connect to the database
+    db = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1],
+                         passwd=sys.argv[2], db=sys.argv[3])
+    # create a cursor object
+    cursor = db.cursor()
+    # execute the query
+    cursor.execute("SELECT * FROM states WHERE name = %s ORDER BY id ASC",
+                   (sys.argv[4],))
+    # fetch all the results
+    results = cursor.fetchall()
+    # print the results
+    for row in results:
+        print(row)
+    # close the database connection
+    db.close()
