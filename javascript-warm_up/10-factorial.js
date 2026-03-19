7acf3e480ed108d71b9b0a0c071cce33d25edfb0
MattW-12106 <12106@holbertonstudents.com>
2026-03-19 13:55:56 +1100

    factorial

--- javascript-warm_up/10-factorial.js
+++ javascript-warm_up/10-factorial.js
@@ -1,11 +1,11 @@
 #!/usr/bin/node
 function factorial(n) {
-    if (n === 0 || n === 1) {
-        return 1;
-    }
-    let result = 1;
-    for (let i = 2; 1 <= n; i++) {
-        result *= i;
-    }
-    return result;
+  if (isNaN(n) || n <= 1) {
+    return 1;
+  }
+  return n * factorial(n - 1);
 }
+
+const n = Number(process.argv[2]);
+
+console.log(factorial(n));
