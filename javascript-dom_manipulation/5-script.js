009f5517f3fcb77a94fb3d03507a43f9c6aa80b6
MattW-12106 <12106@holbertonstudents.com>
2026-03-19 22:33:56 +1100

    updates header when user clicks on element with update header id

---
+++ javascript-dom_manipulation/5-script.js
@@ -0,0 +1,7 @@
+#!/usr/bin/node
+const updateBtn = document.querySelector('#update_header');
+const header = document.querySelector('header');
+
+updateBtn.addEventListener('click', function () {
+  header.textContent = 'New Header!!!';
+});
