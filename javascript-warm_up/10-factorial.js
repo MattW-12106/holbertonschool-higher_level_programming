a43404fd1c46c7bfe9c9633f5f6c0ad6a133b82a
MattW-12106 <12106@holbertonstudents.com>
2026-03-19 13:53:47 +1100

    factorial

---
+++ javascript-warm_up/10-factorial.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+function factorial(n) {
+    if (n === 0 || n === 1) {
+        return 1;
+    }
+    let result = 1;
+    for (let i = 2; 1 <= n; i++) {
+        result *= i;
+    }
+    return result;
+}
